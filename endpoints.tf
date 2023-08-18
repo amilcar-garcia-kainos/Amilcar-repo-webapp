6e512d7a95e9784b5bcac4f9ab669122ccc5f443
Amilcar Garcia Triana <amilcargarciatriana@k7227wf6j3.client.kainos.com>
2023-08-18 16:29:29 +0100

    Updating files

--- endpoints.tf
+++ endpoints.tf
@@ -1,15 +1,15 @@
 # Creating endpoint
-resource "azurerm_private_endpoint" "agt_db_pe" {
-  name                = "amilcar-privateendpoint"
-  location            = azurerm_resource_group.webapp_rg.location
-  resource_group_name = azurerm_resource_group.webapp_rg.name
-  subnet_id           = azurerm_subnet.db_subnet.id
+# resource "azurerm_private_endpoint" "agt_db_pe" {
+#   name                = "amilcar-privateendpoint"
+#   location            = azurerm_resource_group.webapp_rg.location
+#   resource_group_name = azurerm_resource_group.webapp_rg.name
+#   subnet_id           = azurerm_subnet.db_subnet.id
 
-  private_service_connection {
-    name                           = "private-service-connection"
-    private_connection_resource_id = azurerm_postgresql_server.agt_postgresql_server.id
-    subresource_names              = ["postgresqlServer"]
-    is_manual_connection           = false
-  }
+#   private_service_connection {
+#     name                           = "private-service-connection"
+#     private_connection_resource_id = azurerm_postgresql_server.agt_postgresql_server.id
+#     subresource_names              = ["postgresqlServer"]
+#     is_manual_connection           = false
+#   }
 
-}
+# }
--- key_vault.tf
+++ key_vault.tf
@@ -1,22 +1,22 @@
 # Creating the key vault
 
-data "azurerm_client_config" "current"{}
+data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "agtKV" {
-  name                        = "amilcar-key-vault"
-  location                    = azurerm_resource_group.webapp_rg.location
-  resource_group_name         = azurerm_resource_group.webapp_rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  #enable_rbac_authorization   = true
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  sku_name                    = "standard"
+# resource "azurerm_key_vault" "agtKV" {
+#   name                        = "amilcar-key-vault"
+#   location                    = azurerm_resource_group.webapp_rg.location
+#   resource_group_name         = azurerm_resource_group.webapp_rg.name
+#   #enabled_for_disk_encryption = true
+#   tenant_id                   = data.azurerm_client_config.current.tenant_id
+#   #enable_rbac_authorization   = true
+#   soft_delete_retention_days  = 7
+#   #purge_protection_enabled    = false
+#   sku_name                    = "standard"
 
-}
+# }
 
 #resource "azurerm_key_vault_secret" "agtKVS" {
-  #name         = "amilcar-key-vault-secret"
-  #value        = "notsurehere"
- # key_vault_id = azurerm_key_vault.agtKV.id
+#name         = "amilcar-key-vault-secret"
+#value        = "notsurehere"
+# key_vault_id = azurerm_key_vault.agtKV.id
 #}
